b2458a2c63a9dbab3cd780f66ed54343cd3575b4
benisai <59147467+benisai@users.noreply.github.com>
2025-01-20 08:31:55 -0800

    Update 5-minute-script.sh

--- Router/Crontab/5-minute-script.sh
+++ Router/Crontab/5-minute-script.sh
@@ -1,1 +1,14 @@
-5-minute-script.sh
+#!/bin/sh
+
+#---------------------------------------------------------------------------------------------------------#
+# Restart Netify if memory is too high
+if ! pgrep netifyd
+then /etc/init.d/netifyd start
+else
+if [ `top -b -n 1 | grep netify | grep -v "grep" | awk '{print $6}'| tr -d '%'` -gt 25 ];then
+echo "Restarting Netify due to high memory"
+/etc/init.d/netifyd restart
+else
+echo "Netify Memory is fine"
+fi
+fi
