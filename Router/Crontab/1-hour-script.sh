dd17cae918a2714a2422c3f143abbfc4241c9cb8
benisai <59147467+benisai@users.noreply.github.com>
2025-01-20 08:29:07 -0800

    Update 1-hour-script.sh

--- Router/Crontab/1-hour-script.sh
+++ Router/Crontab/1-hour-script.sh
@@ -1,1 +1,11 @@
-1-hour-script.sh
+#!/bin/sh
+
+#---------------------------------------------------------------------------------------------------------#
+#service netifyd restart
+PID=`ps | grep "netifyd" | grep -v "grep" | awk '{print $1}'`
+if test -d /proc/$PID/; then
+    echo Netify Netcat Process Found, killing Process $PID
+    kill -9 $PID 1>&2
+fi
+
+#---------------------------------------------------------------------------------------------------------#
