4424d295d96fd780af2c6d3c2a6196392c3c0090
benisai <59147467+benisai@users.noreply.github.com>
2025-01-23 21:37:51 -0800

    Update and rename nlbw-compare-service.sh to nlbw-compare-rate-service.sh

--- Router/Scripts/nlbw-compare-rate-service.sh
+++ Router/Scripts/nlbw-compare-rate-service.sh
@@ -1,11 +1,10 @@
-##!/bin/sh /etc/rc.common
 #!/bin/sh
 
 # Service name
-SERVICE_NAME="nlbw-compare-service.sh"
+SERVICE_NAME="nlbw-compare-rate-service.sh"
 
 # Path to the main script
-SCRIPT_PATH="/usr/bin/nlbw-compare.sh"
+SCRIPT_PATH="/usr/bin/nlbw-compare-rate.sh"
 
 # File to store the old stats
 OLD_FILE="/tmp/nlbw_old.csv"
@@ -16,7 +15,7 @@ OUTPUT_FILE="/tmp/nlbw_final_compare.txt"
 
 # Start the service
 start() {
-    echo "Starting nlbw-compare.sh service..."
+    echo "Starting nlbw-compare-rate.sh service..."
     
     # Run the script in the background
     /bin/sh "$SCRIPT_PATH" &
@@ -26,10 +25,10 @@ start() {
 
 # Stop the service
 stop() {
-    echo "Stopping nlbw-compare.sh service..."
+    echo "Stopping nlbw-compare-rate.sh service..."
     
     # Find the PIDs of the running process
-    pids=$(ps | grep "[n]lbw-compare.sh" | awk '{print $1}')
+    pids=$(ps | grep "[n]lbw-compare-rate.sh" | awk '{print $1}')
     
     # If PIDs are found, kill each one
     if [ -n "$pids" ]; then
@@ -38,7 +37,7 @@ stop() {
             echo "Stopped process with PID: $pid"
         done
     else
-        echo "No running nlbw-compare.sh process found."
+        echo "No running nlbw-compare-rate.sh process found."
     fi
 }
 
@@ -50,27 +49,27 @@ restart() {
 
 # Enable the service (link to startup directory)
 enable() {
-    echo "Enabling nlbw-compare.sh service..."
+    echo "Enabling nlbw-compare-rate.sh service..."
     # Create symlink to startup directory
-    ln -s /etc/init.d/nlbw-compare-service.sh /etc/rc.d/S99nlbw-compare-service.sh
+    ln -s /etc/init.d/nlbw-compare-rate-service.sh /etc/rc.d/S99nlbw-compare-rate-service.sh
     echo "Service enabled."
 }
 
 # Disable the service (remove symlink)
 disable() {
-    echo "Disabling nlbw-compare.sh service..."
-    rm -f /etc/rc.d/S99nlbw-compare-service.sh
+    echo "Disabling nlbw-compare-rate.sh service..."
+    rm -f /etc/rc.d/S99nlbw-compare-rate-service.sh
     echo "Service disabled."
 }
 
 # Status of the service
 status() {
-    pids=$(ps | grep "[n]lbw-compare.sh" | awk '{print $1}')
+    pids=$(ps | grep "[n]lbw-compare-rate.sh" | awk '{print $1}')
     
     if [ -n "$pids" ]; then
-        echo "nlbw-compare.sh is running with PID: $pids"
+        echo "nlbw-compare-rate.sh is running with PID: $pids"
     else
-        echo "nlbw-compare.sh is not running."
+        echo "nlbw-compare-rate.sh is not running."
     fi
 }
 
