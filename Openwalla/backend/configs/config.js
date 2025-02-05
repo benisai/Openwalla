83be500589551424d5d9c6f09581fb11221071a7
benisai <59147467+benisai@users.noreply.github.com>
2025-02-05 13:43:35 -0800

    Update config.js

--- Openwalla/backend/configs/config.js
+++ Openwalla/backend/configs/config.js
@@ -1,6 +1,6 @@
 const getEnvConfig = () => {
   return {
-    router_ip: process.env.ROUTER_IP || '10.0.3.1',
+    router_ip: process.env.ROUTER_IP || '10.0.5.1',
     netify_port: process.env.NETIFY_PORT || '7150',
     netify_enabled: process.env.NETIFY_ENABLED !== 'false',
     data_plan_limit: process.env.DATA_PLAN_LIMIT || '500',
